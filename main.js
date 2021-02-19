27467efc6559e259d3287288f64a684b2b53ed3f
shaman <vibesintime@gmail.com>
2021-02-19 15:48:06 -0500

    Added boilerplate and some pseudocode

---
+++ main.js
@@ -0,0 +1,35 @@
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+
+
+
+// Make buttons to choose slots for player choices
+
+
+
+// Make play game button
+
+// Play BG music when 'Play Game' button is clicked
+
+// Add event listeners for buttons/gameboard
+
+// Make Win/Lose/Draw message box
+
+// Make current turn/player message box
+
+// Make invisible bottom row of divs for rest of the pieces to start on
+
+// Check for Win/Lose/Draw after each piece is placed
+
+// Make gameboard border turn red when a winner is determined
+
+// Init
+
+// Render 
