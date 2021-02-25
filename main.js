30baefd5d57009b8627fdb0e78cd1960897af1a9
shaman <vibesintime@gmail.com>
2021-02-24 19:18:37 -0500

    Made progress with clicking board listener

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 /*----- constants -----*/
 
+// console.log(players);
 const players = {
   "1": {
     name: "Player 1",
@@ -10,15 +11,20 @@ const players = {
     score: 0
   }
 };
-// console.log(players);
-
 
 
 /*----- app's state (variables) -----*/
-
+let board = [
+  [0,0,0,0,0,0,0], 
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0]
+]
 
 /*----- cached element references -----*/
-
+// let 
 
 /*----- event listeners -----*/
 
@@ -28,12 +34,57 @@ const players = {
 
 
 
+
 // Add event listeners for buttons/gameboard or possibly do an event listener for
 // entire grid so that it eliminates unecessary/extra code and simplifies functions
 
+function spaceClicked(e){
+  if (e.target.className !== 'grid-item') {
+      return 
+  }
+  // console.log('this is e', e.target)  
+  let click = parseInt(e.target.id);
+  let bottomSpace = click % 7 + 35;
+  let secondSpace = click % 7 + 28;
+  let thirdSpace = click % 7 + 21;
+  let fourthSpace = click % 7 + 14;
+  let fifthSpace = click % 7 + 7;
+  let topSpace = click % 7;
+
+  if (board[Math.floor(bottomSpace / 7)][bottomSpace % 7] == 0){
+    board[Math.floor(bottomSpace / 7)][bottomSpace % 7] = 1;
+  } else if(grid[Math.floor(secondSpace / 7)][secondSpace % 7] == 0){
+    board[Math.floor(secondSpace / 7)][secondSpace % 7] = 1;
+  } else if(grid[Math.floor(thirdSpace / 7)][thirdSpace % 7] == 0){
+    board[Math.floor(thirdSpace / 7)][thirdSpace % 7] = 1;
+  } else if(grid[Math.floor(fourthSpace / 7)][fourthSpace % 7] == 0){
+    board[Math.floor(fourthSpace / 7)][fourthSpace % 7] = 1;
+  } else if(grid[Math.floor(fifthSpace / 7)][fifthSpace % 7] == 0){
+    board[Math.floor(fifthSpace / 7)][fifthSpace % 7] = 1;
+  } else if(grid[Math.floor(topSpace / 7)][topSpace % 7] == 0){
+    board[Math.floor(fifthSpace / 7)][fifthSpace % 7] = 1;
+  } else {
+    alert('SELECTION NOT VALID')
+  }
+}
+// console.log(grid)
+// console.log(Math.floor(bottomSpace / 7));
+// console.log(grid[bottomSpace / 7])
 
 
 // addEventListeners as I add functions
+
+// Play BG music when 'Play Game' button is clicked - Icebox
+
+
+// Make Win/Lose/Draw message box
+
+// Make current turn/player message box
+
+// Make invisible bottom row of divs for rest of the pieces to start on | Get this done ASAP | changing this
+
+// Check for Win/Lose/Draw after each piece is placed aka win logic | basically done(want to display win message)
+
 
 // Make play game button | done
 
@@ -48,21 +99,11 @@ playGameButton.addEventListener("click", function() {
 });
 
 // console.log(playGameButton);
-// Play BG music when 'Play Game' button is clicked
-
-
-// Make Win/Lose/Draw message box
-
-// Make current turn/player message box
-
-// Make invisible bottom row of divs for rest of the pieces to start on | Get this done ASAP
-
-// Check for Win/Lose/Draw after each piece is placed aka win logic | basically done(want to display win message)
 
 // make function to sort out win logic and check for a winner as the game progresses | done(needs result to be shown)
-
 function checkWinner() {
   const cells = document.querySelectorAll("grid-container");
+  const winner = document.querySelector("#winner");
   const winCombos = [
     [0, 1, 2, 3], [1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [7, 8, 9, 10], [8, 9, 10, 11], [9, 10, 11, 12],
     [10, 11, 12, 13], [14, 15, 16, 17], [15, 16, 17, 18], [16, 17, 18, 19], [17, 18, 19, 20], [21, 22, 23, 24], 
@@ -87,17 +128,25 @@ function checkWinner() {
     && win2.classList.contains("player1")
     && win3.classList.contains("player1")
     && win4.classList.contains("player1")) {
+
       // code result displayed for player 1 winning
-      
+      winner.innerHTML = "Player 1 has beat you!"
+
     } else if(win1.classList.contains("player2")
     && win2.classList.contains("player2")
     && win3.classList.contains("player2")
     && win4.classList.contains("player2")) {
-// code result displayed for player 2 winning
 
+// code result displayed for player 2 winning
+      winner.innerHTML = "Player 2 has beat you!"
     }
   };
 };
+
+// write code for event listener to check the winner after each piece is chosen
+
+cells.forEach(cells => cells.addEventListener("click", checkWinner))
+
 
 // console.log(cells);
 // console.log(checkWinner);
@@ -105,4 +154,5 @@ function checkWinner() {
 
 // Init
 
+// function init()
 // Render 
